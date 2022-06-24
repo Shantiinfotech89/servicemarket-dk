128dfcf7671037dd785f341e50d7eb8c42f361db
Shanti <developer.shantiinfosoft@gmail.com>
2022-06-24 19:29:00 +0530

    ms 24062022

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, BrowserRouter as Router } from 'react-router-dom';
+import './App.scss';
+
+// import LoginSignup from './pages/login-signup/LoginSignup'
+
+
+function App() {
+
+  return (
+  <Router>
+      <Routes>
+        {/* <Route exact path="/" render={(props) => <LoginSignup {...props} />} /> */}
+        
+      </Routes>
+  </Router>)
+}
+
+export default App;
